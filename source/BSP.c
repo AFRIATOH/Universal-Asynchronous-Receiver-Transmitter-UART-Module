47fa69741548eb918124443bae8312884e20c7da
lior667 <lior9@post.bgu.ac.il>
2022-06-13 14:58:00 +0300

    d

--- source/BSP.c
+++ source/BSP.c
@@ -68,8 +68,8 @@ void UARTconfig(void)
   UCA0BR0 = 104;                           // 
   UCA0BR1 = 0x00;                           //
   UCA0MCTL = UCBRS0;               // 
-  //UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
-  //IE2 |= UCA0RXIE;                          // Enable USCI_A0 RX interrupt
+  UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
+  IE2 |= UCA0RXIE;                          // Enable USCI_A0 RX interrupt
   //__bis_SR_register(LPM0_bits + GIE);       // Enter LPM3 w/ int until Byte RXed
 }
 //******************************************************************
